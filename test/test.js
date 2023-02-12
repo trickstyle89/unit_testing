c659c376c530415ae00935476cbcf85b7a234741
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-12 15:00:51 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
 const chai = require('chai');  // import the chai library
 const assert = chai.expect;  // establish a variable to be used in our tests
 const validator = require('../javascript/groupValidator.js'); // import the file where our function lives
-describe("The function groupValidator()", () => {
-    // this is where we put our tests.
+it("should return true if there are between 2 and 5 group members", () => {
+  assert(validator.isGroupValid(["a", "b", "c", "d", "e"])).to.be.true;
+});
+it("should return true if there are between 2 and 5 group members", () => {
+  assert(validator.isGroupValid(["a", "c", "d", "e"])).to.be.true;
+});
+it("should return true if there are between 2 and 5 group members", () => {
+  assert(validator.isGroupValid(["a", "b", "c"])).to.be.true;
 });
