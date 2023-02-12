1cbd3e76ec1ff23e429222a8236374f911a7ad5a
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-12 12:23:03 -0500

    sunday

---
+++ test/test.js
@@ -0,0 +1,6 @@
+const chai = require('chai');  // import the chai library
+const assert = chai.expect;  // establish a variable to be used in our tests
+const validator = require('../javascript/groupValidator.js'); // import the file where our function lives
+describe("The function groupValidator()", () => {
+    // this is where we put our tests.
+});
